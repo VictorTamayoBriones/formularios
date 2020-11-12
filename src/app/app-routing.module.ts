0877e8264a30febaf24cc398004ae288bfb45552
VictorManuelTamayo <victortamayo509@gmail.com>
2020-11-11 23:09:52 -0600

    routesAndComponents

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveComponent } from './pages/reactive/reactive.component';
+import { TemplatesComponent } from './pages/templates/templates.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'template', component: TemplatesComponent },
+  { path:'reactive', component: ReactiveComponent },
+  { path: '**', pathMatch:'full', redirectTo: 'template' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
